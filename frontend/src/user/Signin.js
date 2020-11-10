6e661040cbb85d2fca7f770d15747798bc866fc0
Namle98 <lephuongnam2807@gmail.com>
2020-11-10 18:16:18 +0700

    Signup

---
+++ frontend/src/user/Signin.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../core/Layout'
+import API from '../config'
+
+const Signin = () => { 
+    return (
+        <Layout title='Signin' description="SIGNIN TO MERN STACK" >
+            ...
+        </Layout>
+    )
+}
+export default Signin
+
